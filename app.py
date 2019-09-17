8de23a2d0c211c51b47197703651024601007c02
ahmedkmadani <ahmedkamal543@gmail.com>
2019-09-17 15:56:13 +0000

    validation

--- app.py
+++ app.py
@@ -139,7 +139,11 @@ def ussd_callback():
         response += "2. Agrovet \n"  
 
     elif text != '1' or text != '2' or text != '3' :
-        response = " END unvalid option"                                            
+        response  = "CON Pick produce \n"
+        response += "1. Red Irish Potatoes \n"
+        response += "2. White Irish Potatoes \n"                                              
+        response += "3. Cowpeas \n"               
+        response += "4. Carrots \n"                                            
 
     return response
 
