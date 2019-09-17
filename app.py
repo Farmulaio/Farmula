b68dfebe10d32a0b26eb39e513a2100b80672a02
ahmedkmadani <ahmedkamal543@gmail.com>
2019-09-17 15:51:22 +0000

    add 3 and combin + text if txt not valid

--- app.py
+++ app.py
@@ -124,7 +124,22 @@ def ussd_callback():
         response += "1. Red Irish Potatoes \n"
         response += "2. White Irish Potatoes \n"                                              
         response += "3. Cowpeas \n"               
-        response += "4. Carrots \n" 
+        response += "4. Carrots \n"
+
+    elif text == '2' :
+        response  = "CON "
+        response += "1. Red Irish Potatoes \n"
+        response += "2. White Irish Potatoes \n"                                              
+        response += "3. Cowpeas \n"               
+        response += "4. Carrots \n"
+    
+    elif text == '3' :
+        response  = "CON Source of seeds : \n"
+        response += "1. Government \n"
+        response += "2. Agrovet \n"  
+
+    elif text != '1' or text != '2' or text != '3' :
+        response = " END unvalid option"                                            
 
     return response
 
