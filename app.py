09ae189edc9b654debe8351bfc874fe797de4a12
ahmedkmadani <ahmedkamal543@gmail.com>
2019-10-19 09:44:05 +0000

    add option for agrovet

--- app.py
+++ app.py
@@ -542,11 +542,13 @@ def ussd_callback():
         response += "3. Cowpeas \n"               
         response += "4. Carrots \n"
 
+# price mechanism for Red Irish
     elif text == "3*1" :
         response  = "CON Source of seeds : \n"
         response += "1. Government \n"
         response += "2. Agrovet \n"  
-    
+
+    # option one for Government
     elif text == "3*1*1":
         response  = "CON Tractor : \n"
         response += "1. Own \n"
@@ -577,6 +579,40 @@ def ussd_callback():
         response += "2. No \n"
     
     elif text == "3*1*1*1*1*2" :
+        response  = "END Your production cost for Red Irish Potatoes per acre is Sh.X \n\n Thank you for using Farmula services"
+
+
+ # option one for Agrovet
+    elif text == "3*1*2":
+        response  = "CON Tractor : \n"
+        response += "1. Own \n"
+        response += "2. Hired \n"
+    
+    elif text == "3*1*2*1" :
+        response  = "CON Source of fertilizer : \n"
+        response += "1. Government \n"
+        response += "2. Agrovet \n" 
+    
+    elif text == "3*1*2*1*1" :
+        response  = "CON Pesticide : \n"
+        response += "1.Yes \n"
+        response += "2. No \n"
+    
+    elif text == "3*1*2*1*1*1" :
+        response  = "END Your production cost for Red Irish Potatoes per acre is Sh.X \n\n Thank you for using Farmula services"
+
+# incase of otption 2 after tractor 
+    elif text == "3*1*2*2" :
+        response  = "CON Source of fertilizer : \n"
+        response += "1. Government \n"
+        response += "2. Agrovet \n" 
+    
+    elif text == "3*1*2*1*2" :
+        response  = "CON Pesticide : \n"
+        response += "1.Yes \n"
+        response += "2. No \n"
+    
+    elif text == "3*1*2*1*1*2" :
         response  = "END Your production cost for Red Irish Potatoes per acre is Sh.X \n\n Thank you for using Farmula services"
 
 
