71eae07b01862d9e1ce496fc5d985a871d6b0492
ahmedkmadani <ahmedkamal543@gmail.com>
2019-11-10 23:14:05 +0000

    fix ussd

--- app.py
+++ app.py
@@ -474,11 +474,11 @@ def ussd_callback():
 
     elif text == '1*1*1':
         response = "CON Place order"
-        response += "\n 1.  " + Kawagware_w50_f
-        response += "\n 2.  " + marikiti_w50_f
-        response += "\n 3.  " + price_w_10kg_peeled
-        response += "\n 4.  " + price_w_20kg_peeled
-        response += "\n 5.  " + price_w_20kg_unpeeled
+        response += "\n 1.  " 
+        # response += "\n 2.  " + marikiti_w50_f
+        # response += "\n 3.  " + price_w_10kg_peeled
+        # response += "\n 4.  " + price_w_20kg_peeled
+        # response += "\n 5.  " + price_w_20kg_unpeeled
 
 
 
