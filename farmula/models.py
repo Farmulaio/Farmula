ab4c6d20271c81bdd45de6ec275ecc2015215e96
ahmedkmadani <ahmedkamal543@gmail.com>
2019-11-19 03:05:17 +0000

    fix ussd

--- farmula/models.py
+++ farmula/models.py
@@ -11,25 +11,6 @@ class Crop(db.Model):
         return f"Crop('{self.IdCrop}','{self.Name},'{self.Enabled}','{self.CreatedAt}')"
 
 
-class Situation(db.Model):
-    IdSituation = db.Column(db.Integer, primary_key=True)
-    Situation  = db.Column(db.String(250), nullable=False)
-    CreatedAt  = db.Column(db.DateTime, nullable=False) 
-
-    def __repr__(self) :
-        return f"Situation('{self.IdSituation}','{self.Situation}','{self.CreatedAt}')"
-
-
-
-class OrderStatus(db.Model):
-    IdOrderStatus = db.Column(db.Integer, primary_key=True)
-    OrderStatus  = db.Column(db.String(250), nullable=False)
-    CreatedAt  = db.Column(db.DateTime, nullable=False) 
-
-    def __repr__(self) :
-        return f"OrderStatus('{self.IdOrderStatus}','{self.OrderStatus}','{self.CreatedAt}')"
-
-
 class Market(db.Model):
     IdMarket = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String(250), nullable=False)
@@ -40,6 +21,7 @@ class Market(db.Model):
 
     def __repr__(self) :
         return f"Market('{self.IdMarket}','{self.Name},'{self.Enabled}','{self.CreatedAt}')"
+
 
 
 class Quantity(db.Model):
@@ -53,55 +35,79 @@ class Quantity(db.Model):
     def __repr__(self) :
         return f"Quantity('{self.IdQty}','{self.Qty},'{self.Enabled}','{self.CreatedAt}')"
 
-class Orders(db.Model):
-    IdOrder = db.Column(db.Integer, primary_key=True)
-    OrderNumber = db.Column(db.String(250), nullable=True)
-    BusinesName = db.Column(db.String(250), nullable=True)
+class Role(db.Model):
+    IdRole = db.Column(db.Integer, primary_key=True)
+    Role = db.Column(db.String(250), nullable=False)
+    CreatedAt = db.Column(db.DateTime, nullable=False) 
+
+    def __repr__(self) :
+        return f"Role('{self.IdRole}','{self.Role},'{self.CreatedAt}')"
+
+class Users(db.Model):
+    IdUser = db.Column(db.Integer, primary_key=True)
+    IdRole = db.Column(db.Integer, db.ForeignKey('role.IdRole'))
+    FirstName = db.Column(db.String(250), nullable=True)
+    LastName = db.Column(db.String(250), nullable=True)
+    Email = db.Column(db.String(250), nullable=True)
     PhoneNumber = db.Column(db.String(250), nullable=True)
     Address = db.Column(db.String(250), nullable=True)
-    IdCrop = db.Column(db.Integer, db.ForeignKey('crop.IdCrop'))
-    IdMarket = db.Column(db.Integer, db.ForeignKey('market.IdMarket'))
-    IdQty = db.Column(db.Integer, db.ForeignKey('quantity.IdQty'))
-    IdOrderStatus  = db.Column(db.Integer, db.ForeignKey('order_status.IdOrderStatus'))
-    Price  = db.Column(db.String(250), nullable=True)
-    CreatedAt = db.Column(db.DateTime, nullable=False)
-    crop = db.relationship("Crop", backref="Orders") 
-    market = db.relationship("Market", backref="Orders")
-    qty = db.relationship("Quantity", backref="Orders")
-    orderstatus = db.relationship("OrderStatus", backref="Orders")
+    Pasword = db.Column(db.String(250), nullable=True)
+    CreatedAt = db.Column(db.DateTime, nullable=False) 
+    Enabled = db.Column(db.Integer, db.ForeignKey('situation.IdSituation'))
+    role = db.relationship('Role',  backref="Users")
+    situation = db.relationship('Situation', backref='Users')
 
 
     def __repr__(self) :
-        return f"Orders('{self.IdOrder}',{self.OrderNumber}','{self.BusinesName}','{self.PhoneNumber}','{self.Address}','{self.IdCrop}','{self.IdMarket}','{self.IdQty}'),'{self.IdOrderStatus}'),'{self.Price}','{self.CreatedAt}')"
+        return f"Users('{self.IdUser}','{self.IdRole},'{self.FirstName}','{self.LastName}','{self.Email}','{self.PhoneNumber}','{self.Address}','{self.Pasword}','{self.CreatedAt}','{self.Enabled}')"        
 
+    def get_id(self):
+        return (self.IdUser)
+
+class Farmer(db.Model):
+    Idfarmer = db.Column(db.Integer, primary_key=True)
+    FirstName = db.Column(db.String(250), nullable=True)
+    LastName = db.Column(db.String(250), nullable=True)
+    PhoneNumber = db.Column(db.String(250), nullable=True)
+    Address = db.Column(db.String(250), nullable=True)
+    IdCrop = db.Column(db.Integer , db.ForeignKey('crop.IdCrop'))
+    Harvestime = db.Column(db.String(250), nullable=True)
+    CreatedAt = db.Column(db.DateTime, nullable=False) 
+    crop = db.relationship('Crop',  backref="Farmer")
+
+    def __repr__(self) :
+        return f"Farmer('{self.Idfarmer}',{self.FirstName}','{self.LastName}','{self.PhoneNumber}','{self.Address}','{self.IdCrop}','{self.Harvestime}','{self.CreatedAt}')"        
+
+
+
+class Business(db.Model):
+    IdBusines = db.Column(db.Integer, primary_key=True)
+    FirstName = db.Column(db.String(250), nullable=True)
+    LastName = db.Column(db.String(250), nullable=True)
+    Email = db.Column(db.String(250), nullable=True)
+    PhoneNumber = db.Column(db.String(250), nullable=True)
+    Address = db.Column(db.String(250), nullable=True)
+    BusinesName = db.Column(db.String(250), nullable=True)
+    CreatedAt = db.Column(db.DateTime, nullable=False) 
+
+    def __repr__(self) :
+        return f"Business('{self.IdBusines}',{self.FirstName}','{self.LastName}','{self.Email}','{self.PhoneNumber}','{self.Address}','{self.BusinesName}','{self.CreatedAt}')"        
 
 class Price(db.Model):
     IdPrice = db.Column(db.Integer, primary_key=True)
     IdCrop = db.Column(db.Integer, db.ForeignKey('crop.IdCrop'))
     IdMarket = db.Column(db.Integer, db.ForeignKey('market.IdMarket'))
     IdQty = db.Column(db.Integer, db.ForeignKey('quantity.IdQty'))
+    IdUser  = db.Column(db.Integer, db.ForeignKey('users.IdUser'))
     Price  = db.Column(db.String(250), nullable=True)
     CreatedAt = db.Column(db.DateTime, nullable=False)
     crop = db.relationship("Crop", backref="Price") 
     market = db.relationship("Market", backref="Price")
     qty = db.relationship("Quantity", backref="Price")
+    user = db.relationship("Users", backref="Price")
 
     def __repr__(self) :
-        return f"Price('{self.IdPrice}',{self.IdCrop}','{self.IdMarket}','{self.IdQty}','{self.Price}','{self.CreatedAt}')"        
-
-
-class Prediction(db.Model):
-    IdPred = db.Column(db.Integer, primary_key=True)
-    IdCrop = db.Column(db.Integer, db.ForeignKey('crop.IdCrop'))
-    PredictionDate  = db.Column(db.String(250), nullable=False)
-    AvgPrice = db.Column(db.String(250), nullable=False)
-    PrePrice  = db.Column(db.String(250), nullable=False)
-    CreatedAt = db.Column(db.DateTime, nullable=False) 
-    crop = db.relationship("Crop", backref="Prediction") 
-
-
-    def __repr__(self) :
-        return f"Prediction('{self.IdPred}','{self.IdCrop},'{self.PredictionDate},'{self.AvgPrice}','{self.PrePrice}','{self.CreatedAt}')"
+        return f"Price('{self.IdPrice}',{self.IdCrop}','{self.IdMarket}','{self.IdQty}','{self.IdUser}','{self.Price}','{self.CreatedAt}')"        
 
 class Pricechecksession(db.Model):
     IdSession = db.Column(db.Integer, primary_key=True)
@@ -122,3 +128,55 @@ class Pricemechsession(db.Model):
 
     def __repr__(self) :
         return f"Pricemechsession('{self.IdSession}','{self.PhoneNumber},'{self.Name}','{self.Hooks}','{self.CreatedAt}')"
+
+class Situation(db.Model):
+    IdSituation = db.Column(db.Integer, primary_key=True)
+    Situation  = db.Column(db.String(250), nullable=False)
+    CreatedAt  = db.Column(db.DateTime, nullable=False) 
+
+    def __repr__(self) :
+        return f"Situation('{self.IdSituation}','{self.Situation}','{self.CreatedAt}')"
+
+class Orders(db.Model):
+    IdOrder = db.Column(db.Integer, primary_key=True)
+    OrderNumber = db.Column(db.String(250), nullable=True)
+    BusinesName = db.Column(db.String(250), nullable=True)
+    PhoneNumber = db.Column(db.String(250), db.ForeignKey('business.PhoneNumber'))
+    Address = db.Column(db.String(250),  nullable=True)
+    IdCrop = db.Column(db.Integer, db.ForeignKey('crop.IdCrop'))
+    IdMarket = db.Column(db.Integer, db.ForeignKey('market.IdMarket'))
+    IdQty = db.Column(db.Integer, db.ForeignKey('quantity.IdQty'))
+    IdOrderStatus  = db.Column(db.Integer, db.ForeignKey('order_status.IdOrderStatus'))
+    Price  = db.Column(db.String(250), nullable=True)
+    CreatedAt = db.Column(db.DateTime, nullable=False)
+    crop = db.relationship("Crop", backref="Orders") 
+    market = db.relationship("Market", backref="Orders")
+    qty = db.relationship("Quantity", backref="Orders")
+    orderstatus = db.relationship("OrderStatus", backref="Orders")
+    business = db.relationship("Business", backref="Orders") 
+
+
+    def __repr__(self) :
+        return f"Orders('{self.IdOrder}',{self.OrderNumber}','{self.BusinesName}','{self.PhoneNumber}','{self.Address}','{self.IdCrop}','{self.IdMarket}','{self.IdQty}'),'{self.IdOrderStatus}'),'{self.Price}','{self.CreatedAt}')"        
+
+class OrderStatus(db.Model):
+    IdOrderStatus = db.Column(db.Integer, primary_key=True)
+    OrderStatus  = db.Column(db.String(250), nullable=False)
+    CreatedAt  = db.Column(db.DateTime, nullable=False) 
+
+    def __repr__(self) :
+        return f"OrderStatus('{self.IdOrderStatus}','{self.OrderStatus}','{self.CreatedAt}')"
+
+
+class Prediction(db.Model):
+    IdPred = db.Column(db.Integer, primary_key=True)
+    IdCrop = db.Column(db.Integer, db.ForeignKey('crop.IdCrop'))
+    PredictionDate  = db.Column(db.String(250), nullable=False)
+    AvgPrice = db.Column(db.String(250), nullable=False)
+    PrePrice  = db.Column(db.String(250), nullable=False)
+    CreatedAt = db.Column(db.DateTime, nullable=False) 
+    crop = db.relationship("Crop", backref="Prediction") 
+
+
+    def __repr__(self) :
+        return f"Prediction('{self.IdPred}','{self.IdCrop},'{self.PredictionDate},'{self.AvgPrice}','{self.PrePrice}','{self.CreatedAt}')"
