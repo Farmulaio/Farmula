2c7e5af4d655e93c1018746f1b786d240f8dc662
ahmedkmadani <ahmedkamal543@gmail.com>
2019-11-19 05:15:03 +0000

    database track set to false

--- farmula/__init__.py
+++ farmula/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'farmula'
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@12345@localhost/farmula"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from farmula import routes
