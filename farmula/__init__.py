726003e444468bafd5959ee7aef385f24bf16941
ahmedkmadani <ahmedkamal543@gmail.com>
2020-01-02 00:30:30 +0000

    chanage database con

--- farmula/__init__.py
+++ farmula/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'farmula'
 socket_location = "/var/run/mysqld/mysqld.sock"
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@12345@localhost/farmula?unix_socket=" + socket_location
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@12345@localhost/farmula?unix_socket=" + socket_location
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@12345@159.203.0.249:3306/farmula?unix_socket=" + socket_location
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
