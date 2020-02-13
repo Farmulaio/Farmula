6076247eefac3c4ea3390fad3b67c781482324e5
ahmedkmadani <ahmedkmadani@Ahmedk-MacBook-Pro.local>
2020-02-13 23:51:08 +0300

    fix database con

--- farmula/__init__.py
+++ farmula/__init__.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'farmula'
 socket_location = "/var/run/mysqld/mysqld.sock"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:mysql@localhost/farmula"
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@12345@159.203.0.249/farmula"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@12345@159.203.0.249/farmula"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@12345@localhost/farmula"
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
