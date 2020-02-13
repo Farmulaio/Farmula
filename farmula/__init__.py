93e24a34f6eae339325367d6098572af74bbb315
ahmedkmadani <ahmedkmadani@Ahmedk-MacBook-Pro.local>
2020-02-14 00:40:50 +0300

    change databae con

--- farmula/__init__.py
+++ farmula/__init__.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'farmula'
 socket_location = "/var/run/mysqld/mysqld.sock"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:mysql@localhost/farmula"
-# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@12345@159.203.0.249/farmula"
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@12345@localhost/farmula"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@12345@159.203.0.249/farmula"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@12345@localhost/farmula"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
