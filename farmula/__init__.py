3c85d65773ad02175096af91ab147187e2a4fc94
Ahmed K Madani <ahmedkamal543@gmail.com>
2020-06-26 16:18:57 +0300

    add package

--- farmula/__init__.py
+++ farmula/__init__.py
@@ -4,10 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, static_folder = 'static', template_folder='templates')
 app.config['SECRET_KEY'] = 'farmula'
 socket_location = "/var/run/mysqld/mysqld.sock"
-# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost/farmula_ke"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost/farmula_ke"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@12345@159.203.0.249/farmula"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@134.209.177.113/farmula"
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@12345@localhost/farmula"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ahmed@12345@localhost/farmula"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
